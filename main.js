df10ee6f8935df96e0a50408217cd3c73800f147
sculpt0r <github@sculpt0r.net>
2021-08-24 12:44:13 +0200

    Add issue getter

---
+++ main.js
@@ -0,0 +1,19 @@
+function getItemEntry() {
+    //output: (GH_LINK)[#ISSUE_NUMBER]: ISSUE_TITLE_WITHOUT_NUMBER
+    const URL = document.URL;
+    const HASH_ELEMENT = document.querySelector('#partial-discussion-header > div.gh-header-show > div > h1 > span.f1-light.color-text-tertiary');
+    const HASH_NUMBER = HASH_ELEMENT.innerHTML;
+
+    const TITLE_ELEMENT = document.querySelector('#partial-discussion-header > div.gh-header-show > div > h1 > span.js-issue-title.markdown-title');
+    const TITLE = TITLE_ELEMENT.innerHTML;
+
+    const result = `[${ URL }](${ HASH_NUMBER }): ${ TITLE }`;
+
+    return result;
+}
+
+const SCULPT0R = {
+    getTMIssueEntry: () => {
+        console.log( getItemEntry() );
+    }
+};
