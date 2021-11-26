d401373a42032813d2622f412b97bb691e9ab5e3
sculpt0r <github@sculpt0r.net>
2021-11-26 21:57:15 +0100

    Rename script file

---
+++ gh-copy-link.js
@@ -0,0 +1,50 @@
+
+function getItemEntry() {
+	//output: (GH_LINK)[#ISSUE_NUMBER]: ISSUE_TITLE_WITHOUT_NUMBER
+	const URL = document.URL;
+	const HASH_ELEMENT = document.querySelector('#partial-discussion-header > div.gh-header-show > div > h1 > span.f1-light.color-text-tertiary');
+	const HASH_NUMBER = HASH_ELEMENT.innerHTML;
+
+	const TITLE_ELEMENT = document.querySelector('#partial-discussion-header > div.gh-header-show > div > h1 > span.js-issue-title.markdown-title');
+	const TITLE = TITLE_ELEMENT.innerHTML;
+
+	const result = `<a href="${ URL }">${ HASH_NUMBER }</a>: ${ TITLE }`;
+
+	return result;
+}
+
+function addHelperButton() {
+
+	const TITLE_HEADER = document.querySelector('#partial-discussion-header > div.gh-header-show > div > h1');
+
+	TITLE_HEADER.insertAdjacentHTML(
+		'afterend',
+		`<button id='sculpt0rGetButton'>GET ENTRY</button>`
+	);
+
+	document.getElementById('sculpt0rGetButton').onclick = function() {
+		SCULPT0R.getTMIssueEntry();
+	}
+
+}
+
+addHelperButton();
+
+window.SCULPT0R = {
+	getTMIssueEntry: () => {
+		var type = "text/html";
+		var blob = new Blob([getItemEntry()], { type });
+		var data = [new ClipboardItem({ [type]: blob })];
+
+		navigator.clipboard.write(data).then(
+			function () {
+				console.log('OK');
+			/* success */
+			},
+			function () {
+				console.log('FAIL');
+			/* failure */
+			}
+		);
+	}
+};
