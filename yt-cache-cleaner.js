e6f7ae4271d5c668a376095338a75b85e96f2bd7
sculpt0r <github@sculpt0r.net>
2021-12-27 23:05:17 +0100

    Clear storage in intervals

--- yt-cache-cleaner.js
+++ yt-cache-cleaner.js
@@ -1,1 +1,9 @@
 
+function clearStorage() {
+	window.sessionStorage.clear();
+	window.indexedDB.clear();
+	window.localStorage.clear();
+	window.caches.clear();
+}
+
+setInterval( clearStorage, 1000 * 60 );
