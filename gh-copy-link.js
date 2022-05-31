0a5cf41d54431126e99c5871abbc77beac5f2d23
sculpt0r <github@sculpt0r.net>
2022-05-31 08:53:00 +0200

    Remove hash anchor from url href

--- gh-copy-link.js
+++ gh-copy-link.js
@@ -1,20 +1,23 @@
 
 function getItemEntry() {
 	//output: (GH_LINK)[#ISSUE_NUMBER]: ISSUE_TITLE_WITHOUT_NUMBER
-	const URL = document.URL;
-	const urlParts = URL.split( '/' );
-	const HASH_NUMBER = urlParts[ urlParts.length - 1 ];
+	const urlObj = new urlHref(document.URL);
+	const urlHref = urlObj.href;
+
+	const urlParts = urlHref.split( '/' );
+	const lastUrlPart =  urlParts[ urlParts.length - 1 ];
+
+	const HASH_NUMBER = lastUrlPart.replace( urlObj.hash, '' );
 
 	const TITLE_ELEMENT = document.querySelector('#partial-discussion-header > div.gh-header-show > div > h1 > span.js-issue-title.markdown-title');
 	const TITLE = TITLE_ELEMENT.innerHTML;
 
-	const result = `<p><a href="${ URL }">#${ HASH_NUMBER }</a>: ${ TITLE }</p>`;
+	const result = `<p><a href="${ urlHref }">#${ HASH_NUMBER }</a>: ${ TITLE }</p>`;
 
 	return result;
 }
 
 function addHelperButton() {
-
 	const TITLE_HEADER = document.querySelector('#partial-discussion-header > div.gh-header-show > div > h1');
 
 	TITLE_HEADER.insertAdjacentHTML(
